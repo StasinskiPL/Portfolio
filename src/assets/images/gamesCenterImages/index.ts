5bf75b66d078dcf1b6739fbf1a7abb42a2a71f6e
GreenDeer2001 <01DawidS@gmail.com>
2020-12-29 19:28:29 +0100

    changing iframe to swiper

---
+++ src/assets/images/gamesCenterImages/index.ts
@@ -0,0 +1,8 @@
+import one from "./1.png";
+import two from "./2.png";
+import three from "./3.png";
+import four from "./4.png";
+import five from ".5.png";
+
+const arr =  [one,two,three,four,five]
+export default arr;
--- src/assets/projects/projects.ts
+++ src/assets/projects/projects.ts
@@ -2,31 +2,38 @@ import GamesCenterImg from "../images/gamesCenter.png";
 import LibraryImg from "../images/library.png";
 import WeatherAppImg from "../images/WeatherApp.png";
 import ExpensTrackerImg from "../images/expenseTracker.png";
-
+import libraryImages from "../images/libraryImages";
+import gamesCenterImages from "../images/gamesCenterImages";
+import weatherAppImages from "../images/weatherAppImages";
+import expenseTrackerImages from "../images/expenseTrackerImages";
 
 interface Project {
   title: string;
   description: string;
   link: string;
   githubLink: string;
-  image: string
+  image: string;
+  detailsImages: Array<string>;
 }
 
 export const projectsList: Array<Project> = [
   {
     title: "GamesCenter",
-    description: "A few two players online mini games with real time chat. Created with React and socket.io ",
+    description:
+      "A few two players online mini games with real time chat. Created with React and socket.io ",
     link: "https://gamescenter-ds.herokuapp.com/",
     githubLink: "https://github.com/StasinskiPL/GamesCenter",
     image: GamesCenterImg,
+    detailsImages: gamesCenterImages,
   },
   {
     title: "Library",
-    description: "Online Library. Created with React , firebase and firebase Authentication.",
+    description:
+      "Online Library. Created with React , firebase and firebase Authentication.",
     link: "https://miejska-biblioteka.netlify.app/",
     githubLink: "https://github.com/StasinskiPL/Biblioteka",
     image: LibraryImg,
-
+    detailsImages: libraryImages,
   },
   {
     title: "WeatherApp",
@@ -34,14 +41,15 @@ export const projectsList: Array<Project> = [
     link: "https://weatherapp-ds.netlify.app/",
     githubLink: "https://github.com/StasinskiPL/WeatherApp",
     image: WeatherAppImg,
-
+    detailsImages: weatherAppImages,
   },
   {
     title: "Expense Tracker",
-    description: "Expense Tracker, my first React App, actually renovated with bootstap. ",
+    description:
+      "Expense Tracker, my first React App, actually renovated with bootstap. ",
     link: "https://github.com/StasinskiPL/GamesCenter",
     githubLink: "https://github.com/StasinskiPL/ExpenseTracker-react",
     image: ExpensTrackerImg,
-
+    detailsImages: expenseTrackerImages,
   },
 ];
