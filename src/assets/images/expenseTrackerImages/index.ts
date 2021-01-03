37e47acd28ba14e6d8651e694fa1a8c733cbfec1
GreenDeer2001 <01DawidS@gmail.com>
2021-01-03 13:01:26 +0100

    expTracker new Images

--- src/assets/images/expenseTrackerImages/index.ts
+++ src/assets/images/expenseTrackerImages/index.ts
@@ -1,6 +1,7 @@
 import one from "./1.png";
 import two from "./2.png";
 import three from "./3.png";
+import four from "./4.png";
 
-const arr =  [one,two,three]
+const arr =  [one,two,three,four]
 export default arr;
